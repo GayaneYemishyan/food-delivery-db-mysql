1648d53fe3e21e5b76c331b393d8ea0dbdf061d4
unknown <gayaneyemishyan4@gmail.com>
2026-04-04 14:41:54 +0400

    Update startup.sh and delete .ebextensions

--- startup.sh
+++ startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Navigate to the UI directory where server.py is located
 cd /home/site/wwwroot/UI || { echo 'Error: UI directory not found'; exit 1; }
-
-# Start the Flask application using gunicorn
-gunicorn --bind=0.0.0.0 --timeout 600 --chdir UI server:app
+gunicorn --bind=0.0.0.0 --timeout 600 server:app
