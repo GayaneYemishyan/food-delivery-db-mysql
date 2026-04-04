fe14d203affc3fd0976c8aff56c8b413bae7fdd6
unknown <gayaneyemishyan4@gmail.com>
2026-04-04 12:56:44 +0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 cd /home/site/wwwroot/UI || { echo 'Error: UI directory not found'; exit 1; }
 
 # Start the Flask application using gunicorn
-gunicorn --bind 0.0.0.0:8000 --workers 4 server:app
+gunicorn --bind=0.0.0.0 --timeout 600 --chdir UI server:app
